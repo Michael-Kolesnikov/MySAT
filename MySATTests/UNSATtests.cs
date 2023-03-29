c04d3055a42a4a384721df56e35488593d78bcea
Michael-Kolesnikov <dark0pesto7@mail.ru>
2023-03-29 23:53:53 +0300

    Fix test

--- MySATTests/UNSATtests.cs
+++ MySATTests/UNSATtests.cs
@@ -7,28 +7,28 @@ namespace MySATTests
         [Test]
         public void Test1()
         {
-            var path = @"..\..\..\TestFiles\aim-50-1_6-no-1.cnf";
+            var path = @"TestFiles\aim-50-1_6-no-1.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "UNSAT");
         }
         [Test]
         public void Test2()
         {
-            var path = @"..\..\..\TestFiles\aim-50-1_6-no-2.cnf";
+            var path = @"TestFiles\aim-50-1_6-no-2.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "UNSAT");
         }
         [Test]
         public void Test3()
         {
-            var path = @"..\..\..\TestFiles\aim-50-1_6-no-3.cnf";
+            var path = @"TestFiles\aim-50-1_6-no-3.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "UNSAT");
         }
         [Test]
         public void Test4()
         {
-            var path = @"..\..\..\TestFiles\aim-50-1_6-no-4.cnf";
+            var path = @"TestFiles\aim-50-1_6-no-4.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "UNSAT");
         }
