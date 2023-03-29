e2e83917a51d3f70418101ae7a342f22c041bcd1
Michael-Kolesnikov <dark0pesto7@mail.ru>
2023-03-30 00:03:05 +0300

    fix pathes

--- MySATTests/SATtests.cs
+++ MySATTests/SATtests.cs
@@ -14,63 +14,63 @@ namespace MySATTests
         [Test]
         public void Test2()
         {
-            var path = @"TestFiles\uf75-02.cnf";
+            var path = @"TestFiles/uf75-02.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test3()
         {
-            var path = @"TestFiles\uf75-03.cnf";
+            var path = @"TestFiles/uf75-03.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test4()
         {
-            var path = @"TestFiles\uf75-04.cnf";
+            var path = @"TestFiles/uf75-04.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test5()
         {
-            var path = @"TestFiles\uf75-05.cnf";
+            var path = @"TestFiles/uf75-05.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test6()
         {
-            var path = @"TestFiles\uf75-06.cnf";
+            var path = @"TestFiles/uf75-06.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test7()
         {
-            var path = @"TestFiles\uf75-07.cnf";
+            var path = @"TestFiles/uf75-07.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test8()
         {
-            var path = @"TestFiles\uf75-08.cnf";
+            var path = @"TestFiles/uf75-08.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test9()
         {
-            var path = @"TestFiles\uf75-09.cnf";
+            var path = @"TestFiles/uf75-09.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
         [Test]
         public void Test10()
         {
-            var path = @"TestFiles\uf75-010.cnf";
+            var path = @"TestFiles/uf75-010.cnf";
             var answer = SAT.Start(path);
             Assert.AreEqual(answer[0], "SAT");
         }
